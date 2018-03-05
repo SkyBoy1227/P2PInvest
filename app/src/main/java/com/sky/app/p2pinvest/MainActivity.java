ea3154421dab74142b72067ec75b3dcabcae572d
ASUS <553728561@qq.com>
2018-03-05 17:56:12 +0800

    主页布局文件

--- app/src/main/java/com/sky/app/p2pinvest/MainActivity.java
+++ app/src/main/java/com/sky/app/p2pinvest/MainActivity.java
@@ -1,19 +1,19 @@
 package com.sky.app.p2pinvest;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created with Android Studio.
- * 描述: ${DESCRIPTION}
+ * 描述: 主页
  * Date: 2018/3/3
  * Time: 15:36
  *
  * @author 晏琦云
  * @version ${VERSION}
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

615b41047647853ed9a0dfcc692b7b3401bb8b18
ASUS <553728561@qq.com>
2018-03-06 15:55:25 +0800

    提供MyApplication类

---
+++ app/src/main/java/com/sky/app/p2pinvest/common/MyApplication.java
@@ -0,0 +1,51 @@
+package com.sky.app.p2pinvest.common;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Handler;
+
+
+/**
+ * Created with Android Studio.
+ * 描述: 自定义Application
+ * Date: 2018/3/6
+ * Time: 15:46
+ *
+ * @author 晏琦云
+ * @version ${VERSION}
+ */
+public class MyApplication extends Application {
+    // 在整个应用执行过程中，需要提供的变量
+
+    /**
+     * 需要使用的上下文对象
+     */
+    public static Context context;
+
+    /**
+     * 需要使用的handler
+     */
+    public static Handler handler;
+
+    /**
+     * 提供主线程对象
+     */
+    public static Thread mainThread;
+
+    /**
+     * 提供主线程对象的id
+     */
+    public static int mainThreadId;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        context = this.getApplicationContext();
+        handler = new Handler();
+        // 实例化当前application的线程即为主线程
+        mainThread = Thread.currentThread();
+        // 获取当前线程的id
+        mainThreadId = android.os.Process.myTid();
+    }
+}

0e81c6ca4999ae1f1897fc6eaa1bc06874d17c7f
ASUS <553728561@qq.com>
2018-03-06 15:02:18 +0800

    修改主页4个tab的布局

--- app/src/main/java/com/sky/app/p2pinvest/activity/MainActivity.java
+++ app/src/main/java/com/sky/app/p2pinvest/activity/MainActivity.java
@@ -9,11 +9,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.sky.app.p2pinvest.R;
@@ -24,7 +21,6 @@ import com.sky.app.p2pinvest.fragment.MoreFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * Created with Android Studio.
@@ -42,32 +38,17 @@ public class MainActivity extends AppCompatActivity {
      */
     private static final int WHAT_RESET_BACK = 1;
 
-    @BindView(R.id.fl_main)
-    FrameLayout flMain;
-    @BindView(R.id.iv_main_home)
-    ImageView ivMainHome;
-    @BindView(R.id.tv_main_home)
-    TextView tvMainHome;
-    @BindView(R.id.ll_main_home)
-    LinearLayout llMainHome;
-    @BindView(R.id.iv_main_invest)
-    ImageView ivMainInvest;
-    @BindView(R.id.tv_main_invest)
-    TextView tvMainInvest;
-    @BindView(R.id.ll_main_invest)
-    LinearLayout llMainInvest;
-    @BindView(R.id.iv_main_me)
-    ImageView ivMainMe;
-    @BindView(R.id.tv_main_me)
-    TextView tvMainMe;
-    @BindView(R.id.ll_main_me)
-    LinearLayout llMainMe;
-    @BindView(R.id.iv_main_more)
-    ImageView ivMainMore;
-    @BindView(R.id.tv_main_more)
-    TextView tvMainMore;
-    @BindView(R.id.ll_main_more)
-    LinearLayout llMainMore;
+    @BindView(R.id.rb_main_home)
+    RadioButton rbMainHome;
+    @BindView(R.id.rb_main_invest)
+    RadioButton rbMainInvest;
+    @BindView(R.id.rb_main_me)
+    RadioButton rbMainMe;
+    @BindView(R.id.rb_main_more)
+    RadioButton rbMainMore;
+    @BindView(R.id.rg_main_bottom)
+    RadioGroup rgMainBottom;
+
 
     private HomeFragment homeFragment;
     private InvestFragment investFragment;
@@ -100,109 +81,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        rgMainBottom.setOnCheckedChangeListener((group, checkedId) -> {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            transaction = fragmentManager.beginTransaction();
+            hideFragments();
+            switch (checkedId) {
+                case R.id.rb_main_home:
+                    // 首页
+                    if (homeFragment == null) {
+                        homeFragment = new HomeFragment();
+                        transaction.add(R.id.fl_main, homeFragment);
+                    }
+                    // 显示当前的Fragment
+                    transaction.show(homeFragment);
+                    break;
+                case R.id.rb_main_invest:
+                    // 投资
+                    if (investFragment == null) {
+                        investFragment = new InvestFragment();
+                        transaction.add(R.id.fl_main, investFragment);
+                    }
+                    // 显示当前的Fragment
+                    transaction.show(investFragment);
+                    break;
+                case R.id.rb_main_me:
+                    // 我的资产
+                    if (meFragment == null) {
+                        meFragment = new MeFragment();
+                        transaction.add(R.id.fl_main, meFragment);
+                    }
+                    // 显示当前的Fragment
+                    transaction.show(meFragment);
+                    break;
+                case R.id.rb_main_more:
+                    // 更多
+                    if (moreFragment == null) {
+                        moreFragment = new MoreFragment();
+                        transaction.add(R.id.fl_main, moreFragment);
+                    }
+                    // 显示当前的Fragment
+                    transaction.show(moreFragment);
+                    break;
+                default:
+                    break;
+            }
+            // 提交事务
+            transaction.commit();
+        });
         // 默认显示首页
-        setSelect(0);
-    }
-
-    @OnClick({R.id.ll_main_home, R.id.ll_main_invest, R.id.ll_main_me, R.id.ll_main_more})
-    public void showTab(View view) {
-        switch (view.getId()) {
-            case R.id.ll_main_home:
-                // 首页
-                setSelect(0);
-                break;
-            case R.id.ll_main_invest:
-                // 投资
-                setSelect(1);
-                break;
-            case R.id.ll_main_me:
-                // 我的资产
-                setSelect(2);
-                break;
-            case R.id.ll_main_more:
-                // 更多
-                setSelect(3);
-                break;
-            default:
-                break;
-        }
-    }
-
-    /**
-     * 提供相应的Fragment的显示
-     *
-     * @param position
-     */
-    private void setSelect(int position) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        transaction = fragmentManager.beginTransaction();
-        hideFragments();
-        resetTag();
-        switch (position) {
-            case 0:
-                if (homeFragment == null) {
-                    homeFragment = new HomeFragment();
-                    transaction.add(R.id.fl_main, homeFragment);
-                }
-                // 显示当前的Fragment
-                transaction.show(homeFragment);
-                // 改变选中项的图片和文本颜色的变化
-                ivMainHome.setImageResource(R.drawable.bottom02);
-                tvMainHome.setTextColor(getResources().getColor(R.color.home_back_selected));
-                break;
-            case 1:
-                if (investFragment == null) {
-                    investFragment = new InvestFragment();
-                    transaction.add(R.id.fl_main, investFragment);
-                }
-                // 显示当前的Fragment
-                transaction.show(investFragment);
-                // 改变选中项的图片和文本颜色的变化
-                ivMainInvest.setImageResource(R.drawable.bottom04);
-                tvMainInvest.setTextColor(getResources().getColor(R.color.home_back_selected));
-                break;
-            case 2:
-                if (meFragment == null) {
-                    meFragment = new MeFragment();
-                    transaction.add(R.id.fl_main, meFragment);
-                }
-                // 显示当前的Fragment
-                transaction.show(meFragment);
-                // 改变选中项的图片和文本颜色的变化
-                ivMainMe.setImageResource(R.drawable.bottom06);
-                tvMainMe.setTextColor(getResources().getColor(R.color.home_back_selected01));
-                break;
-            case 3:
-                if (moreFragment == null) {
-                    moreFragment = new MoreFragment();
-                    transaction.add(R.id.fl_main, moreFragment);
-                }
-                // 显示当前的Fragment
-                transaction.show(moreFragment);
-                // 改变选中项的图片和文本颜色的变化
-                ivMainMore.setImageResource(R.drawable.bottom08);
-                tvMainMore.setTextColor(getResources().getColor(R.color.home_back_selected));
-                break;
-            default:
-                break;
-        }
-        // 提交事务
-        transaction.commit();
-    }
-
-    /**
-     * 重置ImageView和TextView的显示
-     */
-    private void resetTag() {
-        ivMainHome.setImageResource(R.drawable.bottom01);
-        ivMainInvest.setImageResource(R.drawable.bottom03);
-        ivMainMe.setImageResource(R.drawable.bottom05);
-        ivMainMore.setImageResource(R.drawable.bottom07);
-
-        tvMainHome.setTextColor(getResources().getColor(R.color.home_back_unselected));
-        tvMainInvest.setTextColor(getResources().getColor(R.color.home_back_unselected));
-        tvMainMe.setTextColor(getResources().getColor(R.color.home_back_unselected));
-        tvMainMore.setTextColor(getResources().getColor(R.color.home_back_unselected));
+        rgMainBottom.check(R.id.rb_main_home);
     }
 
     /**

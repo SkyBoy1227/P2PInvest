3397079ca9c043ff89a86f68210b8c4eb7644521
ASUS <553728561@qq.com>
2018-03-09 11:59:19 +0800

    重写onTouchEvent方法实现“跨界”布局

--- app/src/main/java/com/sky/app/p2pinvest/ui/MyScrollView.java
+++ app/src/main/java/com/sky/app/p2pinvest/ui/MyScrollView.java
@@ -1,6 +1,7 @@
 package com.sky.app.p2pinvest.ui;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -19,6 +20,16 @@ public class MyScrollView extends ScrollView {
 
     private View childView;
 
+    /**
+     * 上一次y轴方向操作的坐标位置
+     */
+    private int lastY;
+
+    /**
+     * 用于记录临界状态的左、上、右、下
+     */
+    private Rect normal = new Rect();
+
     public MyScrollView(Context context) {
         super(context);
     }
@@ -34,6 +45,7 @@ public class MyScrollView extends ScrollView {
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
+        // 获取子视图
         if (getChildCount() > 0) {
             childView = getChildAt(0);
         }
@@ -41,6 +53,55 @@ public class MyScrollView extends ScrollView {
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
+        if (childView != null) {
+            int eventY = (int) ev.getY();
+            switch (ev.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    // 记录
+                    lastY = eventY;
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    // 微小的偏移量
+                    int dy = eventY - lastY;
+                    if (isNeedMove()) {
+                        if (normal.isEmpty()) {
+                            // 记录了childView的临界状态的左、上、右、下
+                            normal.set(childView.getLeft(), childView.getTop(), childView.getRight(), childView.getBottom());
+                        }
+                        // 重新布局
+                        childView.layout(childView.getLeft(), childView.getTop() + dy / 2, childView.getRight(), childView.getBottom() + dy / 2);
+                    }
+                    // 重新赋值
+                    lastY = eventY;
+                    break;
+                case MotionEvent.ACTION_UP:
+                    break;
+                default:
+                    break;
+            }
+        }
         return super.onTouchEvent(ev);
+    }
+
+    /**
+     * 是否需要移动
+     *
+     * @return
+     */
+    private boolean isNeedMove() {
+        // 获取用户在y轴方向上的偏移量 （上 + 下 -）
+        int scrollY = this.getScrollY();
+        // 获取子视图的高度
+        int childViewMeasuredHeight = childView.getMeasuredHeight();
+        // 获取布局的高度
+        int scrollViewMeasuredHeight = this.getMeasuredHeight();
+        // dy >= 0
+        int dy = childViewMeasuredHeight - scrollViewMeasuredHeight;
+        if (scrollY <= 0 || scrollY >= dy) {
+            // 按照我们自定义的MyScrollView的方式处理
+            return true;
+        }
+        // 其他处在临界范围内的，返回false。即表示，仍按照ScrollView的方式处理
+        return false;
     }
 }

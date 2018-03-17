c69662b34b438f568c9869d74d1bbab27c74fff5
ASUS <553728561@qq.com>
2018-03-17 14:22:38 +0800

    LoginActivity布局的设置

---
+++ app/src/main/java/com/sky/app/p2pinvest/activity/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.sky.app.p2pinvest.activity;
+
+import com.sky.app.p2pinvest.R;
+import com.sky.app.p2pinvest.common.BaseActivity;
+
+/**
+ * Created with Android Studio.
+ * 描述: 登录界面
+ * Date: 2018/3/17
+ * Time: 14:00
+ *
+ * @author 晏琦云
+ * @version ${VERSION}
+ */
+public class LoginActivity extends BaseActivity {
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initTitle() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_login;
+    }
+}

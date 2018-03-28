eda60c3a03bac0897742d2d5be2b0097c67a4891
ASUS <553728561@qq.com>
2018-03-28 12:57:37 +0800

    使用ShareSDK实现分享功能

---
+++ app/src/main/java/com/sky/app/p2pinvest/apshare/ShareEntryActivity.java
@@ -0,0 +1,7 @@
+package com.sky.app.p2pinvest.apshare;
+
+import cn.sharesdk.alipay.utils.AlipayHandlerActivity;
+
+public class ShareEntryActivity extends AlipayHandlerActivity {
+
+}
--- app/src/main/java/com/sky/app/p2pinvest/common/MyApplication.java
+++ app/src/main/java/com/sky/app/p2pinvest/common/MyApplication.java
@@ -3,7 +3,10 @@ package com.sky.app.p2pinvest.common;
 import android.app.Application;
 import android.content.Context;
 import android.os.Handler;
+import android.os.Process;
 import android.support.test.espresso.core.internal.deps.guava.util.concurrent.ThreadFactoryBuilder;
+
+import com.mob.MobSDK;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -58,7 +61,7 @@ public class MyApplication extends Application {
         // 实例化当前application的线程即为主线程
         mainThread = Thread.currentThread();
         // 获取当前线程的id
-        mainThreadId = android.os.Process.myTid();
+        mainThreadId = Process.myTid();
 
         // 手动创建线程池
         ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
@@ -69,5 +72,8 @@ public class MyApplication extends Application {
 
         // 设置未捕获异常的处理器
 //        CrashHandler.getInstance().init();
+
+        // 初始化ShareSDK
+        MobSDK.init(this);
     }
 }
